654dc889824eea2f37307e520675b63a6d238cc5
Md Hamiduzzaman <mdhamiduzzaman@Mds-MacBook-Pro.local>
2020-11-13 15:34:13 +0600

    simple edits

--- docker/start.sh
+++ docker/start.sh
@@ -18,7 +18,7 @@ if [ "$role" = "app" ]; then
 elif [ "$role" = "queue" ]; then
 
     echo "Running the queue..."
-    php "$path/artisan" queue:work --verbose --tries=3 --timeout=90 --sleep=1
+    php "$path/artisan" queue:work --verbose --tries=3 --timeout=90
 
 elif [ "$role" = "scheduler" ]; then
 
